55f361de0e75d9cd40a7b995ce13156b8b945457
coolascool <aniket.787068@gmail.com>
2024-04-27 16:02:12 +0530

    navbar displace

--- Frontend/src/Layout/Layout.jsx
+++ Frontend/src/Layout/Layout.jsx
@@ -1,8 +1,7 @@
 import React from 'react'
-import Footer from '../components/Footer'
-import Navbar from './Navbar'
+import Navbar from '../components/Navbar'
 import { Outlet } from 'react-router-dom'
-
+import Footer from '../components/Footer'
 
 export default function Layout() {
   return (
