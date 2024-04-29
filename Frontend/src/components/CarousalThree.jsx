496c40edea879d97cbc04df451047e29c91f8a98
Tushar Gandhi <gandhitushar509@gamail.com>
2024-04-29 20:40:04 +0530

    resolve error

--- Frontend/src/components/CarousalThree.jsx
+++ Frontend/src/components/CarousalThree.jsx
@@ -1,12 +1,12 @@
 import Carousel from 'react-bootstrap/Carousel';
 import ExampleCarouselImage from './ExampleCarouselImage';
 import C1 from '../assets/contact1.jpg';
-import C2 from '../assets/contact2jpg.jpg';
+import C2 from '../assets/contact2.jpg';
 import C3 from '../assets/contact3.jpg';
 
 function UncontrolledExample() {
   return (
-    <Carousel controls={false} interval={3000} indicators={false}>
+    <Carousel controls={false}  indicators={false}>
       <Carousel.Item className='let-let'>
         <ExampleCarouselImage imageUrl={C1} altText="First slide"/>
         <Carousel.Caption>
