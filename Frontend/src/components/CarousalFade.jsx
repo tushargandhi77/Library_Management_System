9a2a296a5c870db1725a70bc88918d593cec425a
coolascool <aniket.787068@gmail.com>
2024-05-03 21:44:10 +0530

    images start

--- Frontend/src/components/CarousalFade.jsx
+++ Frontend/src/components/CarousalFade.jsx
@@ -1,25 +1,28 @@
 import Carousel from 'react-bootstrap/Carousel';
 import ExampleCarouselImage from './ExampleCarouselImage';
+import L1 from '../assets/second images/slide1.jpg';
+import L2 from '../assets/second images/slide2.jpg';
+import L3 from '../assets/second images/slide3.jpg';
 
 function CarouselFade() {
   return (
     <Carousel fade>
       <Carousel.Item className='let-let'>
-        <ExampleCarouselImage altText="First slide" imageUrl="https://source.unsplash.com/random/900x700/?food"/>
+        <ExampleCarouselImage imageUrl={L1} altText="First slide" />
         <Carousel.Caption>
           <h3>First slide label</h3>
           <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
         </Carousel.Caption>
       </Carousel.Item>
       <Carousel.Item className='let-let'>
-        <ExampleCarouselImage altText="Second slide" imageUrl="https://source.unsplash.com/random/900x700/?burger"/>
+      <ExampleCarouselImage imageUrl={L2} altText="First slide" />
         <Carousel.Caption>
           <h3>Second slide label</h3>
           <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
         </Carousel.Caption>
       </Carousel.Item>
       <Carousel.Item className='let-let'>
-        <ExampleCarouselImage altText="Third slide" imageUrl="https://source.unsplash.com/random/900x700/?magic"/>
+      <ExampleCarouselImage imageUrl={L3} altText="First slide" />
         <Carousel.Caption>
           <h3>Third slide label</h3>
           <p>
