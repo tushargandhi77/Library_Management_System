c41191d9b729c7ac0f1634cbcde755b4e4b0ee8e
coolascool <aniket.787068@gmail.com>
2024-05-08 20:08:46 +0530

    Carousal three

--- Frontend/src/components/CarousalThree.jsx
+++ Frontend/src/components/CarousalThree.jsx
@@ -1,35 +1,21 @@
 import Carousel from 'react-bootstrap/Carousel';
 import ExampleCarouselImage from './ExampleCarouselImage';
 import C1 from '../assets/contact1.jpg';
-import C2 from '../assets/contact2.jpg';
-import C3 from '../assets/contact3.jpg';
+// import C2 from '../assets/contact2.jpg';
+// import C3 from '../assets/contact3.jpg';
 
 function UncontrolledExample() {
   return (
     <Carousel controls={false}  indicators={false}>
       <Carousel.Item className='let-let-let-let-let'>
         <ExampleCarouselImage imageUrl={C1} altText="First slide"/>
-        <Carousel.Caption>
-          {/* <h3>First slide label</h3>
-          <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p> */}
-        </Carousel.Caption>
+      </Carousel.Item>
+      {/* <Carousel.Item className='let-let'>
+        <ExampleCarouselImage imageUrl={C1} altText="First slide"/>
       </Carousel.Item>
       <Carousel.Item className='let-let'>
-        <ExampleCarouselImage imageUrl={C2} altText="First slide"/>
-        <Carousel.Caption>
-          {/* <h3>Second slide label</h3>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p> */}
-        </Carousel.Caption>
-      </Carousel.Item>
-      <Carousel.Item className='let-let'>
-        <ExampleCarouselImage imageUrl={C3} altText="First slide" />
-        <Carousel.Caption>
-          {/* <h3>Third slide label</h3>
-          <p>
-            Praesent commodo cursus magna, vel scelerisque nisl consectetur.
-          </p> */}
-        </Carousel.Caption>
-      </Carousel.Item>
+        <ExampleCarouselImage imageUrl={C1} altText="First slide" />
+      </Carousel.Item> */}
     </Carousel>
   );
 }
--- Frontend/src/components/ContactUs.jsx
+++ Frontend/src/components/ContactUs.jsx
@@ -29,13 +29,13 @@ import { toast } from 'react-toastify';
     return (
         <form ref={form} onSubmit={sendEmail} className="d-flex flex-column align-items-center bg-black-black mt-5 rounded-1">
                  <h1 className="text-white mt-4 mb-4">Send Us Your Query</h1>
-                 <p className="text-white" style={labelStyle}>Name</p>
-                 <input type="text" name="from_name" className="bg-light rounded-2 mb-2" style={inputStyle} />
-                 <p className="text-white" style={labelStyle}>Mobile</p>
-                 <input type="text" className="bg-light rounded-2 mb-2" style={inputStyle} />
-                 <p className="text-white" style={labelStyle}>Email</p>
-                 <input type="text" className="bg-light rounded-2 mb-2" style={inputStyle} />
-                 <p className="text-white" style={{ marginLeft: '-290px' }}>Message</p>
+                 <p className="text-white" style={labelStyle} aria-required>Name</p>
+                 <input type="text" name="from_name" rows="2" className="bg-light rounded-2 mb-2" style={inputStyle} />
+                 <p className="text-white" style={labelStyle} aria-required>Mobile</p>
+                 <input type="text" rows="2" className="bg-light rounded-2 mb-2" style={inputStyle} />
+                 <p className="text-white" style={labelStyle} aria-required>Email</p>
+                 <input type="text" rows="2" className="bg-light rounded-2 mb-2" style={inputStyle} />
+                 <p className="text-white" style={{ marginLeft: '-290px' }} aria-required>Message</p>
                  <textarea name="message" id="" cols="30" rows="4" className="bg-light rounded-2 mb-2" style={inputStyle}></textarea>
                  <button className="mt-2 p-2 rounded-2 text-white bg-primary mb-4" style={buttonStyle} type="submit" value="Send">Send Message</button>
         </form>
